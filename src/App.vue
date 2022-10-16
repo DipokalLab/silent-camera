87daa76c0d455ad4d3b83c23426113dba76f8627
Dipokal HHJ <hhj@devent.kr>
2022-10-16 17:34:50 +0900

    init: setup vue

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="container mx-auto px-4 py-2">
+  <Header />
+
+  <div class="h-full">
+
+  </div>
+
+</div>
+
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+
+export default {
+  name: 'App',
+    components: {
+      Header
+  }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+
+<template>
+  <header>
+    <div class="text-center text-gray-800 py-20 px-6">
+      <h1 class="text-5xl font-bold mt-0 mb-3">Vue Tailwind </h1>
+      <h3 class="text-xl font-normal text-gray-600 mb-6">부트스트랩만 쓰던 나에게 tailwind가 들어왔다.</h3>
+      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
+    </div>
+  </header>
+</template>
